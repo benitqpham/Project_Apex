81f926ba9e5a5e47c798ce639621576663681a7f
benitqpham <benitqpham@gmail.com>
2024-05-23 18:28:40 +0200

    latest change

--- scipt.js
+++ scipt.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Desktop
+let lastScrollTop = 0;
+const navbar = document.querySelector(".grid-header"); // Adjust the selector to match your navigation bar
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        navbar.classList.add('hidden');
+    } else {
+        // Scrolling up
+        navbar.classList.remove('hidden');
+    }
+    lastScrollTop = scrollTop;
+});
